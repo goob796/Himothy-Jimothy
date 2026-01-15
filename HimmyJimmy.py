7afb554f1399fca3f3478bbc248ed3855b578b49
goob796 <gabrielxhines@gmail.com>
2026-01-15 12:12:03 -0600

    Add files via upload

---
+++ HimmyJimmy.py
@@ -0,0 +1,29 @@
+#Team project. Antonio Lucky Duck
+import random
+
+def main():
+    pass
+
+def output_dice(dice):
+    pass
+
+def roll_die():
+    pass
+
+def first_roll():
+    pass
+
+def count_frequency(dice, number):
+    pass
+
+def find_mode(dice):
+    pass
+
+def list_unmatched_dice(dice):
+    pass
+
+def reroll_one(dice, index):
+    pass
+
+def reroll_many(dice):
+    pass
